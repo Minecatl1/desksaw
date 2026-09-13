93fd4b630095305731060f8f39e5fb68e96f2c18
Minecatl1 <140106910+Minecatl1@users.noreply.github.com>
2026-09-12 20:34:18 -0500

    Add gyroGravity console command for Android support

--- Scripts/impo/commands.gd
+++ Scripts/impo/commands.gd
@@ -6,6 +6,14 @@ func _ready():
 	resizeWindow(Vector2(gbData.settings.ConsoleSize.x, gbData.settings.ConsoleSize.y))
 	CommandsGlobal.runInitialCommands.emit()
 	CommandsGlobal.resizeCommandCalled.connect(resizeWindow)
+	
+	# Add gyroGravity command for Android
+	if GlobalVariable.gyroGravityEnabled:
+		Console.create_command("gyroGravity", Callable(self, "_gyro_gravity_command"))
 
 func resizeWindow(v):
 	root.size = v
+
+# Command handler for gyro gravity
+func _gyro_gravity_command(_args):
+	pass
